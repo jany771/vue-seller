cafd13089c1b51573e7774be9b28a6fb0aa796cd
朱娟娟 <zhjnjn93@163.com>
2019-05-22 18:16:23 +0800

    20190622

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-header :seller="seller">我是header</v-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import qs from 'query-string'
+import { getSeller } from 'api'
+import vHeader from 'components/v-header/v-header'
+
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    vHeader
+  },
+  data() {
+    return {
+      seller:{
+         id: qs.parse(location.search).id
+      }
+    }
+  },
+  computed: {
+    
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller(){
+      getSeller({
+        id:this.seller.id,
+      }).then((seller) => {
+        this.seller = Object.assign({}, this.seller, seller)
+      })
+    }
+  },
 }
 </script>
 
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="tab">
+  <cube-tab-bar 
+    :useTransition=false
+    :showSlider=true
+    v-model="selectedLabel"
+    :data="tabs"
+    ref="tabBar"
+    class="border-bottom-1px">
+     
+  </cube-tab-bar>
+  <div class="slide-wrapper">
+   <cube-tab-panels v-model="selectedLabel">
+    <cube-tab-panel v-for="(item, index) in tabs" :label="item.label" :key="item.label">
+      <ul>
+        <li class="tab-panel-li" v-for="(hero, index) in item.heroes">
+          {{hero}}
+        </li>
+      </ul>
+    </cube-tab-panel>
+  </cube-tab-panels>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name:tab,
+  props:{
+    tabs:
+  }
+}
+</script>
